bf99de44b8a3b15e84a46c9bad61ff79663a13ba
Kyle Fuller <inbox@kylefuller.co.uk>
2014-08-23 16:58:23 +0100

    [Tests] Test sending messages to nicks and channels

--- irctk/tests/test_channel.py
+++ irctk/tests/test_channel.py
@@ -1,10 +1,12 @@
 import unittest
 from irctk.channel import Channel
+from irctk.tests.mock_client import MockClient as Client
 
 
 class ChannelTests(unittest.TestCase):
     def setUp(self):
-        self.channel = Channel(None, '#testing')
+        self.client = Client()
+        self.channel = Channel(self.client, '#testing')
 
     def test_channel_has_name(self):
         self.assertEqual(self.channel.name, '#testing')
@@ -14,4 +16,12 @@ class ChannelTests(unittest.TestCase):
 
     def test_channel_repr(self):
         self.assertEqual(repr(self.channel), '<Channel #testing>')
+
+    def test_send_message_to_channel(self):
+        self.channel.send('Hello World')
+        self.assertEqual(self.client.sent_lines, ['PRIVMSG #testing :Hello World'])
+
+    def test_parting_sends_part(self):
+        self.channel.part()
+        self.assertEqual(self.client.sent_lines, ['PART #testing'])
 
--- irctk/tests/test_nick.py
+++ irctk/tests/test_nick.py
@@ -1,10 +1,12 @@
 import unittest
 from irctk.nick import Nick
+from irctk.tests.mock_client import MockClient as Client
 
 
 class NickTests(unittest.TestCase):
     def setUp(self):
-        self.nick = Nick(None, 'kylef', 'kyle', 'kylefuller.co.uk')
+        self.client = Client()
+        self.nick = Nick(self.client, 'kylef', 'kyle', 'kylefuller.co.uk')
 
     def test_nick_initialization(self):
         self.assertEqual(self.nick.nick, 'kylef')
@@ -12,13 +14,13 @@ class NickTests(unittest.TestCase):
         self.assertEqual(self.nick.host, 'kylefuller.co.uk')
 
     def test_nick_parsing_from_userhost(self):
-        nick = Nick.parse(None, 'kylef!kyle@kylefuller.co.uk')
+        nick = Nick.parse(self.client, 'kylef!kyle@kylefuller.co.uk')
         self.assertEqual(nick.nick, 'kylef')
         self.assertEqual(nick.ident, 'kyle')
         self.assertEqual(nick.host, 'kylefuller.co.uk')
 
     def test_nick_parsing_from_hostname(self):
-        nick = Nick.parse(None, 'kylefuller.co.uk')
+        nick = Nick.parse(self.client, 'kylefuller.co.uk')
         self.assertEqual(nick.host, 'kylefuller.co.uk')
 
     def test_conversion_to_string(self):
@@ -26,4 +28,10 @@ class NickTests(unittest.TestCase):
 
     def test_nick_repr(self):
         self.assertEqual(repr(self.nick), '<Nick kylef!kyle@kylefuller.co.uk>')
+
+    #
+
+    def test_send_message_to_nick(self):
+        self.nick.send('Hello World')
+        self.assertEqual(self.client.sent_lines, ['PRIVMSG kylef :Hello World'])
 
