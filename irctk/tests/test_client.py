090b7115d84b715d57e09ab735119f6e383f2fc6
Kyle Fuller <inbox@kylefuller.co.uk>
2014-08-23 17:23:40 +0100

    [Client] Test private messages get sent to delegate

--- irctk/tests/test_client.py
+++ irctk/tests/test_client.py
@@ -1,10 +1,22 @@
 import unittest
 from irctk.tests.mock_client import MockClient as Client
+from irctk.nick import Nick
 
 
 class ClientTests(unittest.TestCase):
     def setUp(self):
         self.client = Client('kylef', 'kyle', 'Kyle Fuller')
+        self.client.nick.nick = 'kylef'
+        self.client.delegate = self
+
+        self.private_messages = []
+
+    # Delegate
+
+    def irc_private_message(self, client, nick, message):
+        self.private_messages.append((client, nick, message))
+
+    # Tests
 
     def test_client_has_nickname(self):
         self.assertEqual(self.client.nickname, 'kylef')
@@ -36,4 +48,11 @@ class ClientTests(unittest.TestCase):
         self.client.read_data(':irc.kylefuller.co.uk 005 kyle :NICKLEN=5 CHANNELLEN=6')
         self.assertEqual(self.client.isupport.maximum_nick_length, 5)
         self.assertEqual(self.client.isupport.maximum_channel_length, 6)
+
+    # Delegate
+
+    def test_client_forwards_private_messages_to_delegate(self):
+        self.client.read_data(':bob!b@irc.kylefuller.co.uk PRIVMSG kylef :Hey')
+        self.assertEqual(self.private_messages,
+            [(self.client, Nick.parse(self.client, 'bob!b@irc.kylefuller.co.uk'), 'Hey')])
 
