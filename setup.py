d9459ec28e86985bd2a00186508cbe089cb0ba0b
Kyle Fuller <kyle@fuller.li>
2017-09-08 15:05:20 +0100

    refactor: Drop Python 2 support

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Communications :: Chat',
         'Topic :: Software Development :: Libraries',
     ]
